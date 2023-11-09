e32246b8441e65c38585a43d0570ae436e660f72
Reinoud van Leeuwen <reinoud.van.leeuwen@itcreation.nl>
2023-11-09 11:42:57 +0100

    remove some cruft

--- molecule/not_tests/test_default.py
+++
@@ -1,6 +0,0 @@
-# molecule/default/tests/test_default.py
-def test_nginx_installed(host):
-    nginx = host.package("nginx")
-    assert nginx.is_installed
-    apache = host.package("apache2")
-    assert apache.is_installed
